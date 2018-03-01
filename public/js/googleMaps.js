7f41f6e6ccc4450e2aa853f5efb642096ee4ed00
Juan Zapien <jmzapien@ucsd.edu>
2018-03-01 10:47:56 -0800

    didn't finish

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,17 @@
 function initMap() {
 	// add your code here
+	var UCSD_ltlng = {lat:32.878892, lng:-117.235702};
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: UCSD_ltlng,
+		zoom: 15
+	});
+
+
+	var marker = new google.maps.Marker({
+		position: UCSD_ltlng,
+		map: map,
+		title: "UCSD"
+	});
+
 }
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,9 +1,13 @@
 // 'use strict';
 
+
+
+
 // Call this function when the page loads (the "ready" event)
+
 $(document).ready(function() {
   initializePage();
-})
+}) 
 
 /*
  * Function that is called when the document is ready.
@@ -15,12 +19,34 @@ function initializePage() {
  	initRSVPForm();
 }
 
+
+$(function(){
+		$("div.box").bind("taphold", tapholdHandler);
+		function tapholdHandler(event){
+			$(event.target).addClass("taphold");
+		}
+});
+
+
+
 // init jQuery gestures  
 function initGestures() {
-	// add gestures listener here
+	$(function(){
+		$(".judge-img").bind("taphold", tapholdHandler);
+
+		function tapholdHandler(event){
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix: " + targetIDPrefix);
+
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
 }
+
+
+
